871a4c76f6ab268efc366f145449dac94e4933c5
Alexandre Sucupira <al3xamcs@gmail.com>
2020-03-08 11:14:31 -0300

    Primeiro commit. Definindo bibliotecas, esqueleto do proejto etc.

---
+++ contents/header.php
@@ -0,0 +1,20 @@
+<header>
+	<nav class='navbar navbar-expand-lg navbar-dark bg-primary'>
+		<a class='navbar-brand' href='#'>PManager</a>
+
+		<div class='collapse navbar-collapse' id='navbarSupportedContent'>
+			<ul class='navbar-nav mr-auto'>
+				<li class='nav-item active'>
+					<a class='nav-link' href='#'>Lista<span class='sr-only'>(current)</span></a>
+				</li>
+				<li class='nav-item'>
+					<a class='nav-link' href='#'>Novo Projeto</a>
+				</li>
+			</ul>
+			<form class='form-inline my-2 my-lg-0'>
+				<input id='tarefa' class='form-control mr-sm-2' type='search' placeholder='Buscar tarefa por título'>
+				<button id='btnTarefa' class='btn btn-dark my-2 my-sm-0' type='submit'>Buscar</button>
+			</form>
+		</div>
+	</nav>
+</header>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>PManager</title>
+	
+		<link rel='stylesheet' href='css/bootstrap-4.4.1.min.css'>
+		<script src='js/jquery-3.4.1.min.js'></script>
+		<script src='js/popper-1.16.0.min.js'></script>
+		<script src='js/bootstrap-4.4.1.min.js'></script>
+	</head>
+	<body class='bg-dark'>
+		<?php require('contents/header.php'); ?>
+		<main class='container text-light'>
+			
+		</main>
+		<?php requiire('contents/modals.php'); ?>
+	</body>
+</html>
